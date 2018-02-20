72e7befb71cab5fae15a9a059627ecb4b88ea6c8
kward1025 <kward1025@gmail.com>
2018-02-19 22:19:22 -0800

    Homework4
    I did it!!!!!

---
+++ lesson_3/homework3.py
@@ -0,0 +1,7 @@
+
+name = input("what is your name?")
+print(name)
+if name == "shariq":
+    print("access granted, Welcome Mr. Torres")
+else:
+    print("access denied")
---
+++ lesson_4/homework4.py
@@ -0,0 +1,11 @@
+
+fullname = input("What is your full name? ")
+print(fullname)
+
+
+username = input("What is your username? ")
+print(username)
+
+print(fullname[1]+fullname[4]+fullname[5]+username[1]+username[6])
+
+
