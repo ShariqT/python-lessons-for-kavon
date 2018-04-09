1709fcf7b337994e24dca160a794c871857497aa
kward1025 <kward1025@gmail.com>
2018-04-08 22:41:47 -0700

    book assignment
    Check out this tip program I wrote.  Im sp proud of myself eventhough it took me hours to figure out!

--- lesson_7/bookassignments.py
+++ lesson_7/bookassignments.py
@@ -1,6 +1,16 @@
-print("I like coding " + "but it's a bit " + "challending at times. " \
-+ "I am willing to " + "fight through the challenge " + "because I love learning " \
-+ "and I look forward " + "to makng lots of money!")
-# dont run this print("money" * 1000000)
-print('\a')
+#complete favorite_food = input("what is your favorite food?")
+#complete print(favorite_food)
+#complete favorite_food2 = input("what is your second favorite food? ")
+#complete print(favorite_food2) 
+#complete print(favorite_food + favorite_food2)
 
+total_bill = input("what's the total bill? ")
+x = float(total_bill)
+tip_fifteen = x*.15
+tip_fifteen = int(tip_fifteen)
+tip_twenty = x*.20
+tip_twenty = int(tip_twenty)
+print("A 15% tip is", int(tip_fifteen), "dollars.")
+print("A 20% tip is", int(tip_twenty), "dollars.")
+print("Your total bill plus a 15% tip is", float(total_bill)+int(tip_fifteen), "dollars.")
+print("Your total bill plus a 20% tip is", float(total_bill)+int(tip_twenty), "dollars")
