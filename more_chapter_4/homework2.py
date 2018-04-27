970aa467237759ded180d546ebc494563d9fcd40
Shariq Torres <shariq.torres@gmail.com>
2018-04-26 21:00:04 -0700

    sorry

---
+++ more_chapter_4/homework2.py
@@ -0,0 +1,5 @@
+# Kavon, write a simple program using a for loop that will print
+# out all of the multiples of 4 from 1 to 40. The output should be
+# like '4', '8', '12', etc...
+# hint: you have to use range() to get this correct
+ 
