8d2541319235b65bef1bf3c085bd2de521117cfd
Shariq Torres <shariq.torres@gmail.com>
2018-05-17 05:50:22 -0700

    homework

---
+++ lesson_9/homework.py
@@ -0,0 +1,13 @@
+# This is good size project and may take a you a couple of days to finish,
+# but ties in a bunch that we have learned so far.
+# I want you to make a program will help me learn my times tables
+# the program should:
+# 1) Ask me which times tables am I trying to learn. 
+# 2) Then it will ask me three random math questions about
+# that times table. So for example, if I enter in that I am trying to
+# learn the 2's time tables, the program will ask me three math questions
+# about the 2's time tables like 2 x 3 or 2 x 8 or 2 x 2. The math questions
+# must be random.  I don't want you ask me the same math questions over and 
+# over. 
+# 3) after answering the questions, it will give me a score, like 
+# "got 2 out 3 answers correct" and then print out the percentage correct.
