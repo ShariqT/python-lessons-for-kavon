f228b4e52d1d884013292399ade7608fe0886290
Shariq Torres <shariq.torres@gmail.com>
2018-05-24 08:31:46 -0700

    more homework

---
+++ lesson_10/homework.py
@@ -0,0 +1,9 @@
+# The homework for this week is going to be going over 
+# for loops again. Take the variable "my_name" and print
+# every other letter. the resulting message should be
+# "hello kavon" in all caps
+
+# hint: you cannot change the my_name variable
+# hint: you will need to use the len() function to find the length of my_name
+my_name = "gHiEmLpLqOm lKwAeViObN"
+
